99b52f114a57a72dfca5461cd69839ab535a352a
Cristhian Cely <cristhiancely7@gmail.com>
2022-05-30 20:23:55 -0500

    Primer commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
+
+  React.useEffect(() => {
+    const noBtn = document.querySelector('#noBtn');
+
+    if(noBtn){
+      noBtn.addEventListener('mouseover', (e) => {
+        const randomX = parseInt(Math.random()*100);
+        const randomY = parseInt(Math.random()*100);
+        noBtn.style.left = randomX + "%";
+        noBtn.style.top = randomY + "%";
+    });
+  }
+  })
+
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="contenedor">
+      <p contenteditable>¿Quieres ser mi Novia?</p>
+      <button id="yesBtn" className='btn btn-dark' onClick={ () => { alert('Sabia que ibas a decir que si uwu') } }>Si</button>
+      <button
+        id="noBtn"
+        className='btn btn-dark'
+       >No</button>
+      </div>
+
+    </>
+    
+
   );
 }
 
